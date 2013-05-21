003a1d14fd4d3e4e869b7d685f787cdbc01bb03f
root <root@bjzw_114_88.sogou-in.domain>
2013-05-21 16:21:25 +0800

    final

--- gitlab-install-el6.sh
+++ gitlab-install-el6.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 # Installer for GitLab on RHEL 6 (Red Hat Enterprise Linux and CentOS)
-# mattias.ohlsson@inprose.com
-#
-# Only run this on a clean machine. I take no responsibility for anything.
-#
-# Submit issues here: github.com/mattias-ohlsson/gitlab-installer
 
 # Define the public hostname
-export GL_HOSTNAME=$HOSTNAME
+export GL_HOSTNAME='10.136.114.88'
 
 # Install from this GitLab branch
-export GL_GIT_BRANCH="5-0-stable"
+export GL_GIT_BRANCH="5-2-stable"
 
-# Define the version of ruby the environment that we are installing for
-export RUBY_VERSION="1.9.3-p392"
 
-# Define MySQL root password
-MYSQL_ROOT_PW=$(cat /dev/urandom | tr -cd [:alnum:] | head -c ${1:-16})
+# Define MySQL gitlab password
+MYSQL_PW='qzdwqsc1'
 
 # Exit on error
 
@@ -35,68 +28,70 @@ echo "### Check OS (we check if the kernel release contains el6)"
 uname -r | grep "el6" || die 1 "Not RHEL or CentOS 6 (el6)"
 
 # Install base packages
-yum -y install git
 
-## Install epel-release
-yum -y install http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum -y groupinstall 'Development Tools'
 
 # Ruby
-## packages (from rvm install message):
-yum -y install patch gcc-c++ readline-devel zlib-devel libffi-devel openssl-devel make autoconf automake libtool bison libxml2-devel libxslt-devel libyaml-devel
 
-## Install rvm (instructions from https://rvm.io)
-curl -L get.rvm.io | bash -s stable
+yum -y install ruby-tcltk vim-enhanced httpd readline readline-devel ncurses-devel gdbm-devel glibc-devel \
+               tcl-devel openssl-devel curl-devel expat-devel db4-devel byacc \
+	       sqlite-devel gcc-c++ libyaml libyaml-devel libffi libffi-devel \
+	       libxml2 libxml2-devel libxslt libxslt-devel libicu libicu-devel \
+	       system-config-firewall-tui python-devel redis sudo mysql-server wget \
+	       mysql-devel crontabs logwatch logrotate sendmail-cf qtwebkit qtwebkit-devel \
+	       perl-Time-HiRes --enablerepo=epel-6-epel
 
-## Load RVM
-source /etc/profile.d/rvm.sh
+curl --progress http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz | tar xz
+cd ruby-1.9.3-p392
+./configure
+make
+make install
 
-## Fix for missing psych
-## *It seems your ruby installation is missing psych (for YAML output).
-## *To eliminate this warning, please install libyaml and reinstall your ruby.
-## Run rvm pkg and add --with-libyaml-dir
-rvm pkg install libyaml
 
-## Install Ruby (use command to force non-interactive mode)
-command rvm install $RUBY_VERSION --with-libyaml-dir=/usr/local/rvm/usr
-rvm use $RUBY_VERSION
 
 ## Install core gems
+
 gem install bundler
 
 # Users
 
 ## Create a git user for Gitlab
-adduser --system --create-home --comment 'GitLab' git
+
+adduser --system --create-home --shell /bin/bash --home-dir /home/git --comment 'GitLab' git
+su - git -c 'git config --global user.name  "GitLab"'
+su - git -c 'git config --global user.email "gitlab@10.136.114.88"'
 
 # GitLab Shell
+cd /home/git
 
 ## Clone gitlab-shell
+
 su - git -c "git clone https://github.com/gitlabhq/gitlab-shell.git"
+while [ $? -ne 0] && [$? -ne 128 ]
+do
+	su - git -c "git clone https://github.com/gitlabhq/gitlab-shell.git"
+done
+
+
+cd gitlab-shell
+su - git -c "cd gitlab-shell;git git checkout v1.4.0"
 
 ## Edit configuration
-su - git -c "cp gitlab-shell/config.yml.example gitlab-shell/config.yml"
+
+su - git -c "cd gitlab-shell;cp config.yml.example config.yml"
 sed -i "s/localhost/$GL_HOSTNAME/g" /home/git/gitlab-shell/config.yml
 
 ## Run setup
-su - git -c "rvm use 1.9.3-p392;gitlab-shell/bin/install"
 
-### Fix wrong mode bits
-chmod 600 /home/git/.ssh/authorized_keys
-chmod 700 /home/git/.ssh
-
-# Database
-
-## Install redis
-yum -y install redis
-
-## Start redis
-service redis start
+su - git -c "cd gitlab-shell;./bin/install"
 
 ## Automatically start redis
 chkconfig redis on
 
-## Install mysql-server
-yum install -y mysql-server
+
+## Start redis
+service redis start
+
 
 ## Turn on autostart
 chkconfig mysqld on
@@ -104,108 +99,123 @@ chkconfig mysqld on
 ## Start mysqld
 service mysqld start
 
+#Create mysql user for Git
+
+echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE USER 'gitlab'@'localhost' IDENTIFIED BY '$MYSQL_PW';" | mysql -u root
+
 ### Create the database
 echo "CREATE DATABASE IF NOT EXISTS gitlabhq_production DEFAULT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci';" | mysql -u root
 
 ## Set MySQL root password in MySQL
-echo "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PW') WHERE User='root'; FLUSH PRIVILEGES;" | mysql -u root
+echo "GRANT SELECT, LOCK TABLES, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON gitlabhq_production.* TO 'gitlab'@'localhost';" | mysql -u root
 
 # GitLab
+cd /home/git
+
 
 ## Clone GitLab
 su - git -c "git clone https://github.com/gitlabhq/gitlabhq.git gitlab"
+while [ $? -ne 0] && [$? -ne 128 ]
+do
+	su - git -c "git clone https://github.com/gitlabhq/gitlabhq.git gitlab"
+done
+
+cd /home/git/gitlab
+
 
 ## Checkout
 su - git -c "cd gitlab;git checkout $GL_GIT_BRANCH"
 
 ## Configure GitLab
 
-cd /home/git/gitlab
 
 ### Copy the example GitLab config
-su git -c "cp config/gitlab.yml.example config/gitlab.yml"
+su - git -c "cd gitlab;cp config/gitlab.yml.example config/gitlab.yml"
 
 ### Change gitlabhq hostname to GL_HOSTNAME
-sed -i "s/  host: localhost/  host: $GL_HOSTNAME/g" config/gitlab.yml
+sed -i "s/  host: localhost/  host: '$GL_HOSTNAME'/g" config/gitlab.yml
 
 ### Change the from email address
 sed -i "s/from: gitlab@localhost/from: gitlab@$GL_HOSTNAME/g" config/gitlab.yml
 
-### Copy the example Unicorn config
-su git -c "cp config/unicorn.rb.example config/unicorn.rb"
+### Change LDAP config
 
-### Listen on localhost:3000
-sed -i "s/^listen/#listen/g" /home/git/gitlab/config/unicorn.rb
-sed -i "s/#listen \"127.0.0.1:8080\"/listen \"127.0.0.1:3000\"/g" /home/git/gitlab/config/unicorn.rb
+chown -R git log/
+chown -R git tmp/
+chmod -R u+rwx  log/
+chmod -R u+rwx  tmp/
+su - git -c "mkdir /home/git/gitlab-satellites"
+su - git -c "mkdir /home/git/gitlab/tmp/pids/"
+su - git -c "mkdir /home/git/gitlab/tmp/sockets/"
+chmod -R u+rwx  tmp/pids/
+chmod -R u+rwx  tmp/sockets/
+su - git -c "mkdir /home/git/gitlab/public/uploads"
+chmod -R u+rwx  public/uploads
+su - git -c "cd gitlab;cp config/puma.rb.example config/puma.rb"
+
 
 ### Copy database congiguration
-su git -c "cp config/database.yml.mysql config/database.yml"
+su - git -c "cd gitlab;cp config/database.yml.mysql config/database.yml"
 
 ### Set MySQL root password in configuration file
-sed -i "s/secure password/$MYSQL_ROOT_PW/g" config/database.yml
+sed -i "1,14s/secure password/$MYSQL_PW/g" config/database.yml
+
+sed -i "1,14s/root/gitlab/g" config/database.yml
 
 # Install Gems
 
 ## Install Charlock holmes
-yum -y install libicu-devel
-gem install charlock_holmes --version '0.6.9'
+
+sed -i "1,1s/https/http/g" /home/git/gitlab/Gemfile
+gem install charlock_holmes --version '0.6.9.4'
 
 ## For MySQL
-yum -y install mysql-devel
-su git -c "bundle install --deployment --without development test postgres"
+
+su - git -c "cd gitlab;bundle install --deployment --without development test postgres"
 
 # Initialise Database and Activate Advanced Features
-su git -c "bundle exec rake gitlab:setup RAILS_ENV=production"
+su - git -c "cd gitlab;bundle exec rake gitlab:setup RAILS_ENV=production"
+
 
 ## Install init script
-curl --output /etc/init.d/gitlab https://raw.github.com/gitlabhq/gitlab-recipes/master/init.d/gitlab-centos
+curl --output /etc/init.d/gitlab https://raw.github.com/gitlabhq/gitlabhq/5-2-stable/lib/support/init.d/gitlab
 chmod +x /etc/init.d/gitlab
 
-## Fix for issue 30
-# bundle not in path (edit init-script).
-# Add after ". /etc/rc.d/init.d/functions" (row 17).
-sed -i "17 a source /etc/profile.d/rvm.sh\nrvm use $RUBY_VERSION" /etc/init.d/gitlab
 
 ### Enable and start
 chkconfig gitlab on
+
+su - git -c  "cd gitlab;bundle exec rake gitlab:check RAILS_ENV=production"
+
 service gitlab start
 
-# Apache
+
+# Nginx
 
 ## Install
-yum -y install httpd
-chkconfig httpd on
+yum -y install nginx-sogou-lua.x86_64
 
 ## Configure
-cat > /etc/httpd/conf.d/gitlab.conf << EOF
-ProxyPass / http://127.0.0.1:3000/
-ProxyPassReverse / http://127.0.0.1:3000/
-ProxyPreserveHost On
-EOF
+curl --output /usr/local/nginx/conf/vhosts/gitlab.conf https://raw.github.com/gitlabhq/gitlabhq/master/lib/support/nginx/gitlab
 
-### Configure SElinux
-setsebool -P httpd_can_network_connect 1
+sed -i "1c\user git;" /usr/local/nginx/conf/nginx.conf
 
+sed -i 's/listen.*/listen 80;/g' /usr/local/nginx/conf/vhosts/gitlab.conf
+sed -i "s/YOUR_SERVER_FQDN/gitlab.sogou.com/g" /usr/local/nginx/conf/vhosts/gitlab.conf
+sed -i "s/\/var\/log\/nginx/\/usr\/local\/nginx\/logs/g" /usr/local/nginx/conf/vhosts/gitlab.conf
 ## Start
-service httpd start
+service nginx start
 
-#  Configure iptables
-
-## Open port 80
-iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-
-## Save iptables
-service iptables save
 
 echo "### Done ###############################################"
 echo "#"
-echo "# You have your MySQL root password in this file:"
-echo "# $GL_INSTALL_ROOT/config/database.yml"
+echo "#Change LDAP config by youself"
+echo "#/home/git/gitlab/config/gitlab.yml"
 echo "#"
 echo "# Point your browser to:" 
 echo "# http://$GL_HOSTNAME (or: http://<host-ip>)"
 echo "# Default admin username: admin@local.host"
 echo "# Default admin password: 5iveL!fe"
 echo "#"
-echo "# Flattr me if you like this! https://flattr.com/profile/mattiasohlsson"
 echo "###"
